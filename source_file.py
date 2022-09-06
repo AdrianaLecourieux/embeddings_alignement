1377cb5493abf69eeb0473997d60b7dbb5e32a9a
AdrianaLecourieux <adriana.lecourieux@gmail.com>
2022-09-06 19:32:07 +0200

    Creation of the folders src and bin with the files source_file.py and bin_files.py

---
+++ source_file.py
@@ -0,0 +1,13 @@
+# Source file
+
+
+
+with open("Sequences/5_3_EXONUCLEASE_1BGXT.fasta") as f:
+    firstline = f.readline().rstrip()
+
+print(firstline)
+
+with open("Sequences/5_3_EXONUCLEASE_1BGXT.fasta") as f:
+    firstline = f.readline()
+
+print(firstline)
