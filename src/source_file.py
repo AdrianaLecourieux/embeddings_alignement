176d2bb3b75ec1e1206db236f77cf303e2c1f665
AdrianaLecourieux <adriana.lecourieux@gmail.com>
2022-09-06 21:54:37 +0200

    test of code on source_file (draft)

--- src/source_file.py
+++ src/source_file.py
@@ -1,13 +1,40 @@
 # Source file
 
 
-
-with open("Sequences/5_3_EXONUCLEASE_1BGXT.fasta") as f:
+"""
+with open("../data/fasta_sequences/5_3_EXONUCLEASE_1BGXT.fasta") as f:
     firstline = f.readline().rstrip()
 
 print(firstline)
+"""
 
-with open("Sequences/5_3_EXONUCLEASE_1BGXT.fasta") as f:
-    firstline = f.readline()
 
-print(firstline)
+l1 = [1,2,3]
+l2 = [4,5,6]
+l3 = [7,8,9]
+
+seq1 = []
+seq1.append(l1)
+seq1.append(l2)
+seq1.append(l3)
+
+
+
+v1 = [1,2,3]
+v2 = [4,5,6]
+v3 = [7,8,9]
+
+seq2 = []
+seq2.append(v1)
+seq2.append(v2)
+seq2.append(v3)
+
+
+print(seq1, seq2)
+
+import numpy as np
+"""from scipy.stats import pearsonr
+corr= pearsonr(seq1, seq2)
+
+print(corr)
+"""
